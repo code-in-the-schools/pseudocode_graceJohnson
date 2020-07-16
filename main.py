2c3c46da8b01abc03a4a83fa006d30e38e99b04d
gracejoh <ganwynj@icloud.com>
2020-07-16 15:37:36 +0000

    .

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# input the variable "character"
-#then print "character" in the diamond pattern
+# take the users input in str form
+a = str(input("type a character: "))
+
+#print diamond line by line
+print("    " + a)
+print("   " + a + " " + a)
+print("  " + a + "   " + a)
+print(" " + a + "     " + a)
+print(a + "       " + a) 
+print(" " + a + "     " + a)
+print("  " + a + "   " + a)
+print("   " + a + " " + a)
+print("    " + a)
